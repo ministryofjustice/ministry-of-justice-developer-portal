90da48ea5f96e8b4a1cf431d17c3b11a91136ecd
spencer.howe <spencer.howe@justice.gov.uk>
2026-04-22 15:45:32 +0100

    feat: more amendments to imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.scss';
+import '../../styles/globals.scss';
 import type { Metadata } from 'next';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
